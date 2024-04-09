6601c1f9f98f0d7c0e899c4597e9dbc5513ffceb
Beck <becksons@bu.edu>
2024-04-08 20:01:37 -0400

    More UI updates/adding dummy data to list view

--- app/src/main/java/com/example/flushhubproto/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/flushhubproto/ui/home/HomeFragment.kt
@@ -103,12 +103,6 @@ class HomeFragment : Fragment() {
         }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-
-
-    }
 
     override fun onDestroyView() {
         super.onDestroyView()
