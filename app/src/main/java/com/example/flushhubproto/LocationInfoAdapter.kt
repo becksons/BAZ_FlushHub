7d0d42f721db06ea0f5a199330e8a1102c74644d
Beck <becksons@bu.edu>
2024-04-12 12:04:14 -0400

    Added dummy location data for recycler list view

--- app/src/main/java/com/example/flushhubproto/LocationInfoAdapter.kt
+++ app/src/main/java/com/example/flushhubproto/LocationInfoAdapter.kt
@@ -7,25 +7,36 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tomtom.R
 
-class LocationInfoAdapter(private val items: List<LocationInfo>) : RecyclerView.Adapter<LocationInfoAdapter.ViewHolder>() {
-
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val addressTextView: TextView = view.findViewById(R.id.address_text_view)
-        val distanceTextView: TextView = view.findViewById(R.id.distance_text_view)
-        val timeTextView: TextView = view.findViewById(R.id.time_text_view)
+class LocationInfoAdapter(private var locationList: List<LocationInfo>) : RecyclerView.Adapter<LocationInfoAdapter.LocationViewHolder>() {
+    fun updateData(newLocationList: List<LocationInfo>) {
+        locationList = newLocationList
+        notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LocationViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.location_info_item, parent, false)
-        return ViewHolder(view)
+        return LocationViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = items[position]
-        holder.addressTextView.text = item.address
-        holder.distanceTextView.text = item.distance
-        holder.timeTextView.text = item.time
+    override fun onBindViewHolder(holder: LocationViewHolder, position: Int) {
+        val location = locationList[position]
+        holder.bind(location)
     }
 
-    override fun getItemCount() = items.size
+    override fun getItemCount() = locationList.size
+
+
+    class LocationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val addressTextView: TextView = itemView.findViewById(R.id.address_text_view)
+        private val distanceTextView: TextView = itemView.findViewById(R.id.distance_text_view)
+        private val timeTextView: TextView = itemView.findViewById(R.id.time_text_view)
+
+
+        fun bind(location: LocationInfo) {
+            addressTextView.text = location.address
+            distanceTextView.text = location.distance
+            timeTextView.text = location.time
+        }
+    }
 }
+
--- app/src/main/java/com/example/flushhubproto/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/flushhubproto/ui/home/HomeFragment.kt
@@ -1,42 +1,64 @@
-
 package com.example.flushhubproto.ui.home
 
+
 import android.Manifest
-import android.content.Intent
+import android.animation.ValueAnimator
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.util.Log
+import android.util.TypedValue
+import android.widget.LinearLayout
+import android.widget.TextView
+
 import androidx.core.app.ActivityCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
+import com.example.flushhubproto.LocationInfoAdapter
 import com.example.tomtom.R
 import com.example.tomtom.databinding.FragmentHomeBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.tomtom.quantity.Distance
+import com.tomtom.sdk.location.GeoLocation
 import com.tomtom.sdk.location.GeoPoint
 import com.tomtom.sdk.location.LocationProvider
+import com.tomtom.sdk.location.OnLocationUpdateListener
 import com.tomtom.sdk.location.android.AndroidLocationProvider
 import com.tomtom.sdk.location.android.AndroidLocationProviderConfig
 import com.tomtom.sdk.map.display.MapOptions
+import com.tomtom.sdk.map.display.TomTomMap
 import com.tomtom.sdk.map.display.camera.CameraOptions
 import com.tomtom.sdk.map.display.image.ImageFactory
 import com.tomtom.sdk.map.display.marker.MarkerOptions
 import com.tomtom.sdk.map.display.ui.MapFragment
-import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.milliseconds
 
 class HomeFragment : Fragment() {
 
     companion object {
         const val REQUEST_LOCATION_PERMISSION = 1
+
+        private val bottomSheetBehaviorCallback = object : BottomSheetBehavior.BottomSheetCallback() {
+                override fun onStateChanged(bottomSheet: View, newState: Int) { /* Handle state change */ }
+                override fun onSlide(bottomSheet: View, slideOffset: Float) { /* Handle sliding */ }
+        }
+
     }
+
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private var androidLocationProvider: LocationProvider? = null
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: LocationInfoAdapter
+    private lateinit var viewModel: HomeViewModel
+    private var isExpanded = false
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -44,6 +66,7 @@ class HomeFragment : Fragment() {
             context = requireContext(),
             config = androidLocationProviderConfig
         )
+
         // Now can use androidLocationProvider safely within fragment
     }
 
@@ -51,7 +74,45 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         requestPermissionsIfNecessary()
 
+//        setupAppBarInteraction()
+
+
+        viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+        setupRecyclerView(binding)
+        observeLocationInfos()
+
+
+
+
+
+
         return binding.root
+    }
+//    private fun setupAppBarInteraction() {
+//        val appBar = binding.nearestLocationRecyclerView.nearestRestAppbar // Make sure you have the correct reference to your AppBar
+//        appBar.setOnClickListener {
+//            if (isExpanded) {
+//                collapseRecyclerView()
+//            } else {
+//                expandRecyclerView()
+//            }
+//            isExpanded = !isExpanded
+//        }
+//    }
+
+
+    private fun setupRecyclerView(binding: FragmentHomeBinding) {
+        recyclerView = binding.nearestLocationRecyclerView.nearestLocationRecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        adapter = LocationInfoAdapter(emptyList())
+        recyclerView.adapter = adapter
+    }
+
+
+    private fun observeLocationInfos() {
+        viewModel.locationInfos.observe(viewLifecycleOwner) { locationInfos ->
+            adapter.updateData(locationInfos)
+        }
     }
 
 
@@ -59,7 +120,6 @@ class HomeFragment : Fragment() {
         if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION)
         } else {
-
             initializeMapWithLocation()
         }
     }
@@ -84,45 +144,48 @@ class HomeFragment : Fragment() {
         minDistance = Distance.meters(20.0)
     )
 
-//    val androidLocationProvider: LocationProvider = AndroidLocationProvider(
-//        context = requireContext(),
-//        config = androidLocationProviderConfig
-//    )
 
 
     private fun initializeMapWithLocation() {
+
         childFragmentManager.beginTransaction()
             .replace(R.id.map_container, mapFragment)
             .commit()
 
-        mapFragment.getMapAsync {tomtomMap ->
+        mapFragment.getMapAsync { tomtomMap ->
             tomtomMap.setLocationProvider(androidLocationProvider)
-        }
-        androidLocationProvider?.enable()
+            androidLocationProvider?.enable()
 
-        viewLifecycleOwner.lifecycleScope.launch {
-            androidLocationProvider?.lastKnownLocation?.position?.let { moveMap(it.latitude,it.longitude ) }
+            val onLocationUpdateListener = OnLocationUpdateListener { location: GeoLocation ->
+                Log.d("Location Update", "Latitude: ${location.position.latitude}, Longitude: ${location.position.longitude}")
+
+                // Move map to the new location
+                moveMap(tomtomMap, location.position.latitude, location.position.longitude)
+            }
+
+            androidLocationProvider?.addOnLocationUpdateListener(onLocationUpdateListener)
         }
     }
 
-    fun moveMap(lat: Double, long: Double){
-        mapFragment.getMapAsync { tomtomMap ->
-            val cameraOptions = CameraOptions(
-                position = GeoPoint(lat, long),
-                zoom = 17.0,
-                tilt = 0.0,
-                rotation = 0.0
-            )
+    private fun moveMap(tomtomMap: TomTomMap, lat: Double, long: Double){
+        val cameraOptions = CameraOptions(
+            position = GeoPoint(lat, long),
+            zoom = 17.0,
+            tilt = 0.0,
+            rotation = 0.0
+        )
 
-            val cds = GeoPoint(lat, long)
-            val markerOptions = MarkerOptions(
-                coordinate = cds,
-                pinImage = ImageFactory.fromResource(R.drawable.bathroom_location_icon)
-            )
+        tomtomMap.moveCamera(cameraOptions)
+    }
 
-            tomtomMap.addMarker(markerOptions)
-            tomtomMap.moveCamera(cameraOptions)
-        }
+    fun markMap(tomtomMap: TomTomMap, lat: Double, long: Double){
+        val loc = GeoPoint(lat, long)
+        val markerOptions = MarkerOptions(
+            coordinate = loc,
+            pinImage = ImageFactory.fromResource(R.drawable.bathroom_location_icon)
+        )
+
+        tomtomMap.addMarker(markerOptions)
     }
 
     //distance and timeaway for display given coords
@@ -134,5 +197,6 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
         androidLocationProvider = null
+
     }
 }
